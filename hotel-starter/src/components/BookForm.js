a3539f8104aa92ff7b96bc7de17e08956d2de9c5
Davron01~ <d.mirabdullayev@mail.ru>
2023-01-22 17:20:07 +0500

    add checkin and checkout date

--- hotel-starter/src/components/BookForm.js
+++ hotel-starter/src/components/BookForm.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+// components
+import AdultsDrobdown from "../components/AdultsDropdown";
+import KidsDrobdown from "../components/KidsDropdown";
+import ChekIn from "../components/CheckIn";
+import ChekOut from "../components/CheckOut";
 
 const BookForm = () => {
-  return <div>BookForm</div>;
+  return (
+    <form className="h-[300px] w-full lg:h-[70px]">
+      <div className="flex flex-col w-full h-full lg:flex-row">
+        <div className="flex-1 border-r">
+          <ChekIn />
+        </div>
+        <div className="flex-1 border-r">
+          <ChekOut />
+        </div>
+        <div className="flex-1 border-r">
+          <AdultsDrobdown />
+        </div>
+        <div className="flex-1 border-r">
+          <KidsDrobdown />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default BookForm;
