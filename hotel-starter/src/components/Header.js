0cc1ac3874262b8f3dcd8a3f20a170ffaec25dae
Davron01~ <d.mirabdullayev@mail.ru>
2023-02-14 21:10:00 +0500

    header ends

--- hotel-starter/src/components/Header.js
+++ hotel-starter/src/components/Header.js
@@ -13,13 +13,39 @@ const Header = () => {
     <header
       className={`${
         header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
-      } fixed z-50 w-full transition-all duration-300`}
+      } fixed z-50 w-full transition-all duration-500`}
     >
-      <div className="container mx-auto">
+      <div className="container mx-auto flex flex-col items-center gap-y-6 lg:flex-row lg:justify-between lg:gap-y-0">
         {/* logo */}
         <a href="/">
-          {header ? <img src={LogoDark} /> : <img src={LogoWhite} />}
+          {header ? (
+            <img className="w-[160px]" src={LogoDark} />
+          ) : (
+            <img className="w-[160px]" src={LogoWhite} />
+          )}
         </a>
+        {/* nav */}
+        <nav
+          className={`${
+            header ? "text-primary" : "text-white"
+          } flex gap-x-4 font-tertiary tracking-[3px] text-[15px]items-center uppercase lg:gap-x-8`}
+        >
+          <a href="#" className="hover:text-accent transition">
+            Home
+          </a>
+          <a href="#" className="hover:text-accent transition">
+            Rooms
+          </a>
+          <a href="#" className="hover:text-accent transition">
+            Restaraunt
+          </a>
+          <a href="#" className="hover:text-accent transition">
+            Spa
+          </a>
+          <a href="#" className="hover:text-accent transition">
+            Contact
+          </a>
+        </nav>
       </div>
     </header>
   );
