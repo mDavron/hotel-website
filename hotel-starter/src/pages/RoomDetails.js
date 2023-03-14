3d0bd2e23e4d3e8ef68f0e1c7421a6de3aacf5f4
Davron01~ <d.mirabdullayev@mail.ru>
2023-03-14 20:41:20 +0500

    m

--- hotel-starter/src/pages/RoomDetails.js
+++ hotel-starter/src/pages/RoomDetails.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+//components
+import AdultsDropdown from "../components/AdultsDropdown";
+import KidsDropdown from "../components/KidsDropdown";
+import CheckIn from "../components/CheckIn";
+import CheckOut from "../components/CheckOut";
+//scroll top component
+import ScrollToTop from "../components/ScrollToTop";
+//context
+import { RoomContext } from "../context/RoomContext";
+//icons
+import { FaCheck } from "react-icons/fa";
 
 const RoomDetails = () => {
-  return <div>RoomDetails</div>;
+  const { rooms } = useContext(RoomContext);
+  const { id } = useParams();
+
+  //get room
+  const room = rooms.find((room) => {
+    return room.id === Number(id);
+  });
+  console.log(room);
+  return <div className="bg-pink-200">RoomDetails</div>;
 };
 
 export default RoomDetails;
